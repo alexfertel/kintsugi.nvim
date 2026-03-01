43271f982cc52830b26b2d3ae195b260c785d875
Alexander Gonzalez <alexfertel97@gmail.com>
2026-03-01 21:48:01 +0100

    fix: properly highlight raw string delimiters

--- queries/rust/highlights.scm
+++ queries/rust/highlights.scm
@@ -6,4 +6,14 @@
 ; Keep Rust string quote delimiters on the punctuation track.
 (string_literal
   "\"" @punctuation.quote
-  "\"" @punctuation.quote)
+  "\"" @punctuation.quote
+  (#set! priority 130))
+
+; Raw string delimiters (r#", r###", "#, "###) should stay on punctuation.
+((raw_string_literal) @punctuation.quote
+  (#set! priority 130))
+
+; Keep raw string inner content on the regular string track.
+((raw_string_literal
+  (string_content) @string)
+  (#set! priority 140))
